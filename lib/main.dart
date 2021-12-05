2c7ed625707effb88a45a6bfd0236e9dac110a4d
jacionepalves <jacionepalves@icloud.com>
2021-12-05 09:16:36 -0300

    Projeto My place Admin. Após dividi-lo não consigo rodar a build.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:my_place_admin/core/theme.dart';
-import 'package:my_place_admin/pages/sign_in_page.dart';
-import 'core/theme.dart';
-
+import 'package:my_place_admin/pages/sign_in/sign_in_page.dart';
+import 'package:my_place_core/core/theme.dart';
+import 'package:oktoast/oktoast.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized(); 
@@ -14,11 +13,13 @@ void main() async{
 class MyApp extends StatelessWidget {
     @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme,
-      home: SignInPage(),
+    return OKToast(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: lightTheme,
+        home: SignInPage(),
+      ),
     );
   }
 }
--- lib/pages/sign_up/sign_up_controller.dart
+++ lib/pages/sign_up/sign_up_controller.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_place_core/core/exceptions/email_em_uso_exception.dart';
+import 'package:my_place_core/core/exceptions/email_invalido_exception.dart';
+import 'package:my_place_core/core/exceptions/senha_fraca_exception.dart';
 
-class SignUpController{
+class SignUpController {
   String _nome = '';
   String _email = '';
   String _senha = '';
@@ -18,20 +21,37 @@ class SignUpController{
     }
     return null;
   }
-  Future<void> criaUsuario() async{
-    final userCredencial = await _firebaseAuth.createUserWithEmailAndPassword(
-      email: _email, 
-      password: _senha,
-    );
-    await _userRef.doc(userCredencial.user.uid).set({
-      'nome' : _nome,
-      'email' : _email,
-      'tipo' : 'ADMIN',
-    });
+
+  Future<void> criaUsuario() async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: _email,
+        password: _senha,
+      );
+      await _userRef.doc(userCredential.user.uid).set({
+        'nome': _nome,
+        'email': _email,
+        'tipo': 'ADMIN',
+      });
+    } on Exception catch (e) {
+      if(e is FirebaseAuthException){
+        if (e.code == 'invalid-email'){
+          throw EmailInvalidoException();
+        } else if(e.code == 'weak-password'){
+          throw SenhaFracaException();
+        } else if(e.code == 'email-already-in-use'){
+          throw EmailEmUsoException();
+        }   
+      } else{
+        rethrow;
+      }
+    }
   }
-  
+
   void setNome(String nome) => _nome = nome;
   void setEmail(String email) => _email = email;
   void setSenha(String senha) => _senha = senha;
   void setIsLoading(bool isLoading) => _isLoading = isLoading;
+
+  bool get isLoading => _isLoading;
 }
